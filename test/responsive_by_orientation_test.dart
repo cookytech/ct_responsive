9f3c66622dec8d58e1a6f63e8a13fc187604ae39
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-07-05 13:45:46 +0530

    test: adds tests for whenOrientationLandscape and and whenOrientationPortrait extension functions

--- test/responsive_by_orientation_test.dart
+++ test/responsive_by_orientation_test.dart
@@ -1,5 +1,5 @@
-import 'package:ct_responsive/src/custom_widgets/responsive_by_orientation.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:ct_responsive/ct_responsive.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ct_test_helpers/ct_test_helpers.dart';
 
@@ -39,6 +39,60 @@ main() {
       );
       final findLandscapeText = find.text("landscape");
       expect(findLandscapeText, findsOneWidget);
+    });
+  });
+
+  group("Extension functions", () {
+    group("whenOrientationLandscape", () {
+      testWidgets("shows original when portrait", (WidgetTester tester) async {
+        final originalWidget = Text("original");
+        final landscapeWidget = Text("landscape");
+        tester.setDeviceSize(Size(320, 800));
+        await tester.pumpWidget(originalWidget
+            .whenOrientationLandscape(landscapeWidget)
+            .wrapWithMaterialApp());
+
+        final originalWidgetFinder = find.text("original");
+        expect(originalWidgetFinder, findsOneWidget);
+      });
+
+      testWidgets("shows landscape when landscape",
+          (WidgetTester tester) async {
+        final originalWidget = Text("original");
+        final landscapeWidget = Text("landscape");
+        tester.setDeviceSize(Size(800, 320));
+        await tester.pumpWidget(originalWidget
+            .whenOrientationLandscape(landscapeWidget)
+            .wrapWithMaterialApp());
+
+        final landscapeWidgetFinder = find.text("landscape");
+        expect(landscapeWidgetFinder, findsOneWidget);
+      });
+    });
+
+    group("whenOrientationPortrait", () {
+      testWidgets("shows original when landscape", (WidgetTester tester) async {
+        final originalWidget = Text("original");
+        final portraitWidget = Text("portrait");
+        tester.setDeviceSize(Size(800, 320));
+        await tester.pumpWidget(originalWidget
+            .whenOrientationPortrait(portraitWidget)
+            .wrapWithMaterialApp());
+        final originalWidgetFinder = find.text("original");
+        expect(originalWidgetFinder, findsOneWidget);
+      });
+
+      testWidgets("shows portraitWidget when portrait",
+          (WidgetTester tester) async {
+        final originalWidget = Text("original");
+        final portraitWidget = Text("portrait");
+        tester.setDeviceSize(Size(320, 800));
+        await tester.pumpWidget(originalWidget
+            .whenOrientationPortrait(portraitWidget)
+            .wrapWithMaterialApp());
+        final originalWidgetFinder = find.text("portrait");
+        expect(originalWidgetFinder, findsOneWidget);
+      });
     });
   });
 }
